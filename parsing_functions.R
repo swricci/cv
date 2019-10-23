e3ffb52320aee3a9144fc2e029779506a36b07e0
Shannon Ricci <swbrown@ncsu.edu>
2019-10-23 11:04:20 -0400

    updated cv and resume files, added function for positions with no description

--- parsing_functions.R
+++ parsing_functions.R
@@ -87,6 +87,54 @@ print_section <- function(position_data, section_id){
     )
 }
 
+#S.Ricci added this function
+#if entry has no description
+# Take a position dataframe and the section id desired
+# and prints the section to markdown. 
+print_section_nodesc <- function(position_data, section_id){
+  position_data %>% 
+    filter(section == section_id) %>% 
+    arrange(desc(end)) %>% 
+    mutate(id = 1:n()) %>% 
+    # pivot_longer(
+    #   starts_with('description'),
+    #   names_to = 'description_num',
+    #   values_to = 'description',
+    #   values_drop_na = TRUE
+    # ) %>% 
+    # group_by(id) %>% 
+    # mutate(
+    #   descriptions = list(description)
+    # ) %>% 
+    # ungroup() %>% 
+    # filter(description_num == 'description_1') %>% 
+    mutate(
+      timeline = ifelse(
+        is.na(start) | start == end,
+        end,
+        glue('{end} - {start}')
+      ),
+      #description_bullets = map_chr(descriptions, ~paste('-', ., collapse = '\n')),
+    ) %>% 
+    strip_links_from_cols(c('title')) %>% 
+    mutate_all(~ifelse(is.na(.), 'N/A', .)) %>% 
+    glue_data(
+      "### {title}",
+      "\n\n",
+      "{loc}",
+      "\n\n",
+      "{institution}",
+      "\n\n",
+      "{timeline}", 
+      "\n\n",
+      #"{description_bullets}",
+      "\n\n\n",
+    )
+}
+
+
+
+
 # Construct a bar chart of skills
 build_skill_bars <- function(skills, out_of = 5){
   bar_color <- "#969696"
